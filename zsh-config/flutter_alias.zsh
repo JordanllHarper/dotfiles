af2e45964c19767d863551c40654f0232bfa1a3a
JordanH <jordanh@apadmi.com>
2024-04-15 09:59:45 +0100

    add clean dart build

--- zsh-config/flutter_alias.zsh
+++ zsh-config/flutter_alias.zsh
@@ -1,1 +1,4 @@
 alias db="fvm dart run build_runner build --delete-conflicting-outputs"
+# clean dart build 
+alias cdb="fvm flutter clean && fvm dart run build_runner build --delete-conflicting-outputs"
+
