fcb3225c157829e7ac8a9aebfb25448853a4ec69
JordanH <jordanh@apadmi.com>
2024-05-21 09:26:08 +0100

    update more config

---
+++ .dart-cli-completion/zsh-config.zsh
@@ -0,0 +1,6 @@
+
+## [fvm]
+## Completion config for "fvm"
+[[ -f /Users/jordanh/.config/.dart-cli-completion/fvm.zsh ]] && . /Users/jordanh/.config/.dart-cli-completion/fvm.zsh || true
+## [/fvm]
+
