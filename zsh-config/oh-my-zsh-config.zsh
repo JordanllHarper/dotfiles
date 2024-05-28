ed7a63e16c0a33dc501494606dd66c91a5ecb402
JordanH <jordanh@apadmi.com>
2024-05-28 20:13:32 +0100

    waiting dots

--- zsh-config/oh-my-zsh-config.zsh
+++ zsh-config/oh-my-zsh-config.zsh
@@ -41,9 +41,7 @@ HYPHEN_INSENSITIVE="true"
 
 # Uncomment the following line to display red dots whilst waiting for completion.
 # You can also set it to another string to have that shown instead of the default red dots.
-# e.g. COMPLETION_WAITING_DOTS="%F{yellow}waiting...%f"
-# Caution: this setting can cause issues with multiline prompts in zsh < 5.7.1 (see #5765)
- COMPLETION_WAITING_DOTS="true"
+COMPLETION_WAITING_DOTS="%F{yellow}waiting...%f"
 
 # Uncomment the following line if you want to disable marking untracked files
 # under VCS as dirty. This makes repository status check for large repositories
