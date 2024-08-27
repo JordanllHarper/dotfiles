9b18139d4a543b97259e26fa48b6615ab06a46db
Jordan <46637683+JordanllHarper@users.noreply.github.com>
2024-08-27 18:52:49 +0100

    migrate whichkey mappings

--- nvim/lua/plugins/config/whichkey.lua
+++ nvim/lua/plugins/config/whichkey.lua
@@ -1,20 +1,20 @@
-local opts = {}
+local wk = require("which-key")
 local registers = {
-  ['<leader>b'] = { name = '[B]uffer', _ = 'which_key_ignore' },
-  ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-  ['<leader>d'] = { name = '[D]isplay', _ = 'which_key_ignore' },
-  ['<leader>nf'] = { name = '[F]...', _ = 'which_key_ignore' },
-  ['<leader>f'] = { name = '[F]lutter', _ = 'which_key_ignore' },
-  ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
-  ['<leader>l'] = { name = '[L]azy', _ = 'which_key_ignore' },
-  ['<leader>L'] = { name = '[L]sp', _ = 'which_key_ignore' },
-  ['<leader>o'] = { name = '[O]verseer', _ = 'which_key_ignore' },
-  ['<leader>O'] = { name = '[O]il', _ = 'which_key_ignore' },
-  ['<leader>fr'] = { name = '[R]e...', _ = 'which_key_ignore' },
-  ['<leader>q'] = { name = '[Q]uickfix', _ = 'which_key_ignore' },
-  ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-  ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-  ['<leader>t'] = { name = '[T]erminal', _ = 'which_key_ignore' },
-  ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
+  { '<leader>b',  group = '[B]uffer', },
+  { '<leader>c',  group = '[C]ode', },
+  { '<leader>d',  group = '[D]isplay', },
+  { '<leader>nf', group = '[F]...', },
+  { '<leader>f',  group = '[F]lutter', },
+  { '<leader>g',  group = '[G]it', },
+  { '<leader>l',  group = '[L]azy', },
+  { '<leader>L',  group = '[L]sp', },
+  { '<leader>o',  group = '[O]verseer', },
+  { '<leader>O',  group = '[O]il', },
+  { '<leader>fr', group = '[R]e...', },
+  { '<leader>q',  group = '[Q]uickfix', },
+  { '<leader>r',  group = '[R]ename', },
+  { '<leader>s',  group = '[S]earch', },
+  { '<leader>t',  group = '[T]erminal', },
+  { '<leader>w',  group = '[W]orkspace', },
 }
-require 'which-key'.register(registers, opts)
+wk.add(registers)
