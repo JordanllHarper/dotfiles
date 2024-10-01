f494655264067e892753dd66651845e45cb0c6a7
Jordan <46637683+JordanllHarper@users.noreply.github.com>
2024-10-01 08:58:57 +0100

    remove autocomplete

--- zsh/setup.zsh
+++ zsh/setup.zsh
@@ -16,8 +16,6 @@ export ZSH_CONFIG="$XDG_CONFIG_HOME/zsh"
 source "$ZSH_CONFIG/oh-my-zsh-config.zsh"
 
 # PLUGINS
-source "/opt/homebrew/share/zsh-autocomplete/zsh-autocomplete.plugin.zsh"
-
 # zinit
 if [[ ! -f $HOME/.local/share/zinit/zinit.git/zinit.zsh ]]; then
     print -P "%F{33} %F{220}Installing %F{33}ZDHARMA-CONTINUUM%F{220} Initiative Plugin Manager (%F{33}zdharma-continuum/zinit%F{220})…%f"
