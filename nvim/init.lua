1f180f82d3ef18e5d338f63ff9b8c4a41923ad34
JordanH <jordanh@apadmi.com>
2024-10-04 02:35:42 +0100

    Rename augroups to autocommands
    Rename augroups to autocommands

--- nvim/init.lua
+++ nvim/init.lua
@@ -55,6 +55,6 @@ vim.cmd [[colorscheme kanagawa]]
 leaderNmap("ln", "<Cmd>Lazy<CR>", "[L]azy [N]vim")
 leaderNmap("lr", ":Lazy reload", "[L]azy reload")
 require('keymaps.keymaps')
-require('additional-config.augroups')
+require('additional-config.autocommands')
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
--- nvim/lua/additional-config/autocommands.lua
+++ nvim/lua/additional-config/autocommands.lua
@@ -1,4 +1,5 @@
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+
 vim.api.nvim_create_autocmd('TextYankPost', {
 	callback = function()
 		vim.highlight.on_yank()
@@ -6,3 +7,4 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 	group = highlight_group,
 	pattern = '*',
 })
+
