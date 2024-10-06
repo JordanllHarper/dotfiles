c352ee0c72eb8e081d013ea782bc5202489eaa42
JordanH <jordanh@apadmi.com>
2024-10-06 15:14:11 +0100

    Remove zinit

--- zsh/setup.zsh
+++ zsh/setup.zsh
@@ -15,29 +15,6 @@ export ZSH_CONFIG="$XDG_CONFIG_HOME/zsh"
 # oh my zsh setup
 source "$ZSH_CONFIG/oh-my-zsh-config.zsh"
 
-# PLUGINS
-# zinit
-if [[ ! -f $HOME/.local/share/zinit/zinit.git/zinit.zsh ]]; then
-    print -P "%F{33} %F{220}Installing %F{33}ZDHARMA-CONTINUUM%F{220} Initiative Plugin Manager (%F{33}zdharma-continuum/zinit%F{220})…%f"
-    command mkdir -p "$HOME/.local/share/zinit" && command chmod g-rwX "$HOME/.local/share/zinit"
-    command git clone https://github.com/zdharma-continuum/zinit "$HOME/.local/share/zinit/zinit.git" && \
-        print -P "%F{33} %F{34}Installation successful.%f%b" || \
-        print -P "%F{160} The clone has failed.%f%b"
-fi
-
-source "$HOME/.local/share/zinit/zinit.git/zinit.zsh"
-autoload -Uz _zinit
-(( ${+_comps} )) && _comps[zinit]=_zinit
-
-# Load a few important annexes, without Turbo
-# (this is currently required for annexes)
-zinit light-mode for \
-    zdharma-continuum/zinit-annex-as-monitor \
-    zdharma-continuum/zinit-annex-bin-gem-node \
-    zdharma-continuum/zinit-annex-patch-dl \
-    zdharma-continuum/zinit-annex-rust
-
-
 # TOOLS 
 eval "$(zoxide init zsh)"
 eval "$(opam config env)"
@@ -53,6 +30,8 @@ alias nvimcfg='nvim ~/.config/nvim/'
 alias cfg="nvim ~/.config/"
 alias ndir='nvim ~/notes/'
 alias nnew='nvim ~/notes/skel_note.md'
+
+alias nv="nvim"
 
 # android
 alias shake="adb emu sensor set acceleration 100:100:100; sleep 1; adb emu sensor set acceleration 0:0:0"
