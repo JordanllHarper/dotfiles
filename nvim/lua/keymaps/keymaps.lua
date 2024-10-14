579f19021eb75afbff2b99b61f07f8662e22bb94
Jordan <46637683+JordanllHarper@users.noreply.github.com>
2024-10-14 08:51:47 +0100

    Add cursor moving functionality

--- nvim/lua/keymaps/keymaps.lua
+++ nvim/lua/keymaps/keymaps.lua
@@ -5,6 +5,7 @@ local map = mapUtils.map
 -- Quickfix
 leaderNmap('qo', '<Cmd>copen<CR>', '[Q]uickfix [O]pen')
 leaderNmap('qc', '<Cmd>cclose<CR>', '[Q]uickfix [C]lose')
+
 -- Terminal
 leaderNmap('t', '<Cmd>term<CR>', '[T]erminal')
 map('t', '<C-q>', '<C-\\><C-n>', '[Q]uit terminal mode')
@@ -13,6 +14,11 @@ map('t', '<C-q>', '<C-\\><C-n>', '[Q]uit terminal mode')
 leaderNmap('Li', '<Cmd>LspInfo<CR>', '[L]sp [I]nfo')
 leaderNmap('Lr', '<Cmd>LspRestart<CR>', '[L]sp [R]estart')
 
+-- Cursor Moving
+map("n", "m", "<Cmd>move +1<CR>",  "Move line Up")
+map("n", "<S-m>", "<Cmd>move -2<CR>",  "Move line Down")
+
+-- Resourcing files for config
 leaderNmap("S", function()
 	vim.cmd('luafile %')
 	vim.notify("Resourcing luafile", vim.log.levels.INFO)
