32de356c0221298ba2955a185a06ea39082aa601
JordanH <jordanh@apadmi.com>
2024-10-18 09:45:57 +0100

    Update lock

--- nvim/lua/plugins/config/rustaceanvim.lua
+++ nvim/lua/plugins/config/rustaceanvim.lua
@@ -1,10 +1,8 @@
 local utils = require'utils'
 local leaderNmap = utils.leaderNmap
 
-
 leaderNmap("Rt", "<Cmd>RustTest!<CR>", "[R]ust [T]est")
 leaderNmap("Ra", "<Cmd>RustAnalyzer restart<CR>", "[R]ust [A]nalyzer restart")
-
 
 vim.g.rustaceanvim = {
   server = {
