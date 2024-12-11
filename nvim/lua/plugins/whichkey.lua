578e9e75293761125f466350716e9b3c5c4edc90
JordanH <jordanh@apadmi.com>
2024-12-11 18:23:21 +0000

    Clean up whichkey

--- nvim/lua/plugins/whichkey.lua
+++ nvim/lua/plugins/whichkey.lua
@@ -1,6 +1,22 @@
 return {
-  'folke/which-key.nvim',
-  config = function()
-    require 'plugins.config.whichkey'
-  end
+	"folke/which-key.nvim",
+	opts = {
+		preset = "classic", -- or "modern" normally
+		spec = {
+			{ "<leader>c", group = "[c]ode" },
+			{ "<leader>D", group = "[D]ebug" },
+			{ "<leader>Ds", group = "[s]tep..." },
+			{ "<leader>g", group = "[g]it" },
+			{ "<leader>l", group = "[l]azy" },
+			{ "<leader>L", group = "[L]sp" },
+			{ "<leader>n", group = "[N]eotest" },
+			{ "<leader>O", group = "[O]il" },
+			{ "<leader>q", group = "[q]uickfix" },
+			{ "<leader>r", group = "[r]ename" },
+			{ "<leader>R", group = "[R]ust" },
+			{ "<leader>s", group = "[s]earch" },
+			{ "<leader>t", group = "[t]erminal" },
+			{ "<leader>w", group = "[w]orkspace" },
+		},
+	},
 }
