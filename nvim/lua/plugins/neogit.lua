8592e82e8037337d835889dc2d6c3da14449cea1
JordanH <jordanh@apadmi.com>
2024-12-17 16:35:33 +0000

    Add neogit config

--- nvim/lua/plugins/neogit.lua
+++ nvim/lua/plugins/neogit.lua
@@ -6,11 +6,19 @@ return {
 		"sindrets/diffview.nvim",
 		"nvim-telescope/telescope.nvim",
 	},
+	opts = {
+		kind = "split",
+		disable_line_numbers = false,
+		disable_relative_line_numbers = false,
+		commit_editor = {
+			kind = "vsplit"
+		},
+	},
 	config = function(_, opts)
 		local ng = require("neogit")
 		ng.setup(opts)
 		leaderNmap("go", function()
-			ng.open({ kind = "split" })
+			ng.open()
 		end, "[g]it [o]pen")
 	end,
 }
