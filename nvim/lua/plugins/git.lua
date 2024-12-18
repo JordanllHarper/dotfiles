cba09b5943d58d994ff6f73e66550aeb7d20f00a
JordanH <jordanh@apadmi.com>
2024-12-18 13:06:56 +0000

    Alter to prevent conflict

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -5,6 +5,7 @@ return {
 		"tpope/vim-fugitive",
 		config = function()
 			leaderNmapCmd("gc", "Git commit", "[g]it [C]ommit")
+			leaderNmapCmd("G", "Git", "[g]it [C]ommit")
 		end,
 	},
 	{
@@ -14,6 +15,7 @@ return {
 				local gs = require("gitsigns")
 				leaderNmap("gh", gs.preview_hunk, "[g]it [h]unk")
 				leaderNmap("gs", gs.stage_hunk, "[g]it [s]tage hunk")
+				leaderNmap("gr", gs.reset_hunk, "[g]it [r]eset hunk")
 				leaderNmap("gS", gs.stage_buffer, "[g]it [S]tage buffer")
 			end,
 		},
--- nvim/lua/plugins/put_at_end.lua
+++ nvim/lua/plugins/put_at_end.lua
@@ -4,7 +4,7 @@ return {
 	lazy = false,
 	config = function()
 		local at_end = require("put_at_end")
-		imap("<C-s>", at_end.put_semicolon)
+		imap("<C-p>", at_end.put_semicolon)
 		imap("<C-a>", at_end.put_comma)
 	end,
 }
