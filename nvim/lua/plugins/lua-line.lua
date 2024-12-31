b195b8d66d2d0485b0d74dbff4a38fc24181bc50
Jordan <46637683+JordanllHarper@users.noreply.github.com>
2024-12-31 15:36:39 +0000

    Move lualine configuration into 1 file

--- nvim/lua/plugins/lua-line.lua
+++ nvim/lua/plugins/lua-line.lua
@@ -3,7 +3,36 @@ return {
   dependencies = {
     'nvim-tree/nvim-web-devicons',
   },
-  config       = function()
-    require 'plugins.config.lua-line'
-  end
+  opts         = {
+    options = {
+      icons_enabled = true,
+      theme = 'kanagawa',
+      component_separators = { left = '', right = '' },
+      section_separators = { left = '', right = '' },
+    },
+    extensions = { 'lazy', 'fugitive' },
+    sections = {
+      lualine_a = {
+        {
+          'mode',
+          fmt = function(displayed_mode)
+            return displayed_mode:sub(1, 1)
+          end,
+        }
+      },
+      lualine_b = {
+        { 'diff' },
+      },
+      lualine_c = {
+        { 'buffers', },
+      },
+      lualine_x = {
+        { 'diagnostics' },
+      },
+      lualine_y = { { 'branch', }, },
+      lualine_z = { { 'location', },
+        { 'progress' },
+      },
+    },
+  },
 }
