1b16d3c92c149ae9667cb2ab523582e0d5c5427c
JordanH <jordanh@apadmi.com>
2025-01-08 17:12:41 +0000

    Update blink

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -41,18 +41,17 @@ return {
       { 'L3MON4D3/LuaSnip', version = 'v2.*' }
     },
 
-    version = '0.8.1',
+    version = '*',
 
     opts = {
       snippets = {
-        expand = function(snippet) require('luasnip').lsp_expand(snippet) end,
-        active = function(filter)
-          if filter and filter.direction then
-            return require('luasnip').jumpable(filter.direction)
-          end
-          return require('luasnip').in_snippet()
-        end,
-        jump = function(direction) require('luasnip').jump(direction) end,
+        preset = 'luasnip',
+      },
+      completion = {
+        documentation = {
+          auto_show = true,
+          auto_show_delay_ms = 100,
+        },
       },
       keymap = {
         preset = 'default',
@@ -64,7 +63,7 @@ return {
         nerd_font_variant = 'mono'
       },
       sources = {
-        default = { 'lsp', 'path', 'snippets', 'buffer', 'luasnip' },
+        default = { 'lsp', 'path', 'snippets', 'buffer' },
       },
       signature = { enabled = true }
     },
