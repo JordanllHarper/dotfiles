ad6885f69434e43b67198f337167e49984944edb
JordanH <jordanh@apadmi.com>
2025-02-20 15:22:43 +0000

    Flutter utils

--- nvim/lua/flutter-utils.lua
+++ nvim/lua/flutter-utils.lua
@@ -1,29 +1,23 @@
+---@module "flutter-tools"
+
 ---@class FlutterUtilsConfig
----@field create_run_configurations fun(flavors : string[], user_opts : UserOpts?) : RunConfig[]
+---@field create_run_configurations fun(flavors : string[], user_opts : UserOpts?) : flutter.ProjectConfig[]
 ---@field emulators table<string, string>
 ---
 ---@type FlutterUtilsConfig
 local M = {}
 
----@class RunConfig
----@field name string
----@field flavor string
----@field device string
----@field dart_define_from_file string?
----@field target string?
 
 ---@class UserOpts
 ---@field flavors string[] flavors supported
 ---@field emulators table<string, string>? list of platform and emulators
----@field config_callback fun(cfg: RunConfig) : RunConfig a callback to be run before registering the config
----@field generate_flavor_run_targets boolean whether targets should be generated based on a flavor naming scheme
+---@field config_callback fun(cfg: flutter.ProjectConfig) : flutter.ProjectConfig a callback to be run before registering the config
+---@field generate_flavor_run_targets boolean? whether targets should be generated based on a flavor naming scheme. Defaults to False
 --- E.g. having a flavor "dev" and setting this to true will create a target "lib/main_dev.dart"
----@field emulators table<string, string>?
----@field dart_define_from_file string?
----@field config_callback (fun(cfg: RunConfig) : RunConfig)?
----@field generate_flavor_run_targets boolean
-
-
+---
+---@field dart_define_from_file? string json file to source
+---@field dart_define? {[string]: string}
+---@field config_callback? fun(cfg: flutter.ProjectConfig) : flutter.ProjectConfig
 
 ---@param flavor string
 ---@return string
@@ -32,7 +26,7 @@ local function create_flavor_run_target(flavor)
 end
 
 ---@param user_opts UserOpts? any specific run configurations and device overrides
----@return RunConfig[]
+---@return flutter.ProjectConfig[]
 function M.create_run_configuration(user_opts)
 	local emulators = M.emulators
 	if user_opts and user_opts.emulators ~= nil then
@@ -51,11 +45,13 @@ function M.create_run_configuration(user_opts)
 			if user_opts and user_opts.generate_flavor_run_targets == true then
 				target = create_flavor_run_target(flavor)
 			end
+			---@type flutter.ProjectConfig
 			local config = {
 				name = ("%s - %s"):format(flavor, name),
 				flavor = flavor,
 				target = target,
 				device = device,
+				dart_define_from_file = user_opts and user_opts.dart_define_from_file
 			}
 
 			if user_opts and user_opts.config_callback then
