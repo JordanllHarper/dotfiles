0dbc474a09599063d568c95ff4dbc1366e84a395
Jordan <46637683+JordanllHarper@users.noreply.github.com>
2025-04-08 16:33:08 +0100

    Ghost text enable

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -52,6 +52,9 @@ return {
         preset = 'luasnip',
       },
       completion = {
+        ghost_text = {
+          enabled = true,
+        },
         documentation = {
           auto_show = true,
           auto_show_delay_ms = 100,
