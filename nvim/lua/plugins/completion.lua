30d11d3d60eb55b201c07dde7c3174e6ae434c61
JordanH <jordanh@apadmi.com>
2025-04-15 13:59:22 +0100

    Enable friendly snippets

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -1,7 +1,7 @@
 return {
   {
     "L3MON4D3/LuaSnip",
-    dependencies = { "rafamadriz/friendly-snippets", enabled = false },
+    dependencies = "rafamadriz/friendly-snippets",
     version = "v2.*",
     build = "make install_jsregexp",
     config = function()
